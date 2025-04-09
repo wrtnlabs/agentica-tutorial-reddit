45a0eddb135ad3185845e0eb20f4b0b71c09b9a8
michael <7471919@naver.com>
2025-04-09 15:08:25 +0900

    fix: fix the IProps

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,34 @@
-import {Agentica} from "@agentica/core";
+import { Agentica } from "@agentica/core";
 import dotenv from "dotenv";
-import {OpenAI} from "openai";
+import { OpenAI } from "openai";
 import typia from "typia";
 
-import {RedditService} from "@wrtnlabs/connector-reddit";
+import { RedditService } from "@wrtnlabs/connector-reddit";
 
 dotenv.config();
 
 const agent: Agentica<"chatgpt"> = new Agentica({
-    model: "chatgpt",
-    vendor: {
-        api: new OpenAI({apiKey: process.env.OPENAI_API_KEY!}),
-        model: "gpt-4o-mini",
+  model: "chatgpt",
+  vendor: {
+    api: new OpenAI({ apiKey: process.env.OPENAI_API_KEY! }),
+    model: "gpt-4o-mini",
+  },
+  controllers: [
+    {
+      name: "Reddit Connector",
+      protocol: "class",
+      application: typia.llm.application<RedditService, "chatgpt">(),
+      execute: new RedditService({
+        redditRefreshToken: process.env.REDDIT_REFRESH_TOKEN!,
+        redditClientId: process.env.REDDIT_CLIENT_ID!,
+        redditClientSecret: process.env.REDDIT_CLIENT_SECRET!,
+      }),
     },
-    controllers: [
-        {
-            name: "Reddit Connector",
-            protocol: "class",
-            application: typia.llm.application<RedditService, "chatgpt">(),
-            execute: new RedditService({
-                secret: process.env.REDDIT_SECRET!,
-                clientSecret: process.env.REDDIT_CLIENT_SECRET!,
-                clientId: process.env.REDDIT_CLIENT_ID!,
-            }),
-        },
-    ],
+  ],
 });
 
 const main = async () => {
-    console.log(await agent.conversate("What can you do?"));
+  console.log(await agent.conversate("What can you do?"));
 };
 
 main();
